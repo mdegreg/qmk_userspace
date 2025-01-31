7f877f9269481f64f54a157317218761b38c525d
Matthew DeGregorio <mdegregorio01@gmail.com>
2025-01-30 19:45:46 -0600

    remove right hand nav layout toggle interfering with lbrace

--- keyboards/moonlander/keymaps/warpotato/layout_user.h
+++ keyboards/moonlander/keymaps/warpotato/layout_user.h
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,           KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                KC_Y,                           KC_EQUAL,   KC_U,        KC_I,                      KC_O,        KC_P,    KC_BSLS,        KC_VOLU,
     KC_CAPS_LOCK,       KC_A,           KC_S,           KC_D,               TD(DNC_FIND),   KC_G,                KC_H,                           KC_MINUS,   KC_J,        KC_K,                      KC_L,        KC_SCLN, KC_QUOTE,       KC_VOLD,
     SC_LSPO,            KC_Z,           TD(DNC_XCUT),   TD(DNC_COPY),       TD(DNC_CPS),    KC_B,                                                            KC_N,        KC_M,                      KC_COMMA,    KC_DOT,  KC_SLASH,       SC_RSPC,
-    TT(NUMKEYS_LAYOUT), KC_DEL,         TD(DNC_RTN_L0), TT(NAV_LAYOUT), LT(FKEYS_LAYOUT, KC_TAB),           TD(DNC_BACKSPACE),              TT(NUMKEYS_LAYOUT),      LT(FKEYS_LAYOUT, KC_SCLN), LT(NAV_LAYOUT, KC_LBRC), KC_RBRC, KC_TRANSPARENT, KC_LGUI,
+    TT(NUMKEYS_LAYOUT), KC_DEL,         TD(DNC_RTN_L0), TT(NAV_LAYOUT), LT(FKEYS_LAYOUT, KC_TAB),           TD(DNC_BACKSPACE),              TT(NUMKEYS_LAYOUT),      LT(FKEYS_LAYOUT, KC_SCLN),     KC_LBRC, KC_RBRC, KC_TRANSPARENT, KC_LGUI,
                                                         TD(DNC_SPACE),      MT(MOD_LCTL, KC_ENTER),   TD(DNC_SUPER_ALT_TAB),                    MT(MOD_LALT, KC_QUOTE),  MT(MOD_LCTL, KC_EQUAL),    MT(MOD_LSFT, KC_SPC)
   ),
   [OS_MAC_LAYOUT] = LAYOUT_moonlander(
