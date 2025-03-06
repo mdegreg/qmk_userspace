86c4cd34790d83991da4d86bcd77ac9eff13d402
matt <dev@m.degregor.io>
2025-03-06 00:26:08 -0600

    testing out going back to a slightly more sane touch typing setup

--- keyboards/moonlander/keymaps/warpotato/layout_user.h
+++ keyboards/moonlander/keymaps/warpotato/layout_user.h
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       // baseline layout - mostly qwerty, with some keys shifted to emphasize
       // left hand typing + habitual movements
     TD(DNC_ESC_LS),     KC_1,           KC_2,           KC_3,               KC_4,           KC_5,                KC_TRANSPARENT,                           KC_TRANSPARENT,    KC_6,    KC_7,        KC_8,                      KC_9,        KC_0, KC_TRANSPARENT,
-    KC_GRAVE,           KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                KC_Y,                           KC_BSLS,     KC_EQUAL,   KC_U,        KC_I,                      KC_O,        KC_P,        KC_VOLU,
-    KC_CAPS_LOCK,       KC_A,           KC_S,           KC_D,               TD(DNC_FIND),   KC_G,                KC_H,                           KC_QUOTE,  KC_MINUS,   KC_J,        KC_K,                      KC_L,        KC_SCLN,       KC_VOLD,
+    KC_GRAVE,           KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                KC_TRANSPARENT,                           KC_BSLS,     KC_Y,   KC_U,        KC_I,                      KC_O,        KC_P,        KC_VOLU,
+    KC_CAPS_LOCK,       KC_A,           KC_S,           KC_D,               TD(DNC_FIND),   KC_G,                KC_TRANSPARENT,                           KC_QUOTE,  KC_H,   KC_J,        KC_K,                      KC_L,        KC_SCLN,       KC_VOLD,
     SC_LSPO,            KC_Z,           TD(DNC_XCUT),   TD(DNC_COPY),       TD(DNC_CPS),    KC_B,                                                            KC_SLASH,   KC_N,        KC_M,                      KC_COMMA,    KC_TRANSPARENT,      SC_RSPC,
     TT(NUMKEYS_LAYOUT), KC_LALT,        TD(DNC_RTN_L0), TT(NAV_LAYOUT),     MO(FKEYS_LAYOUT),           TD(DNC_BACKSPACE),                  TD(DNC_BACKSPACE),      MO(FKEYS_LAYOUT), LT(NAV_LAYOUT, KC_LBRC), KC_RBRC, KC_RALT, KC_LGUI,
                                                         TD(DNC_SPACE),      MT(MOD_LCTL, KC_ENTER),   MT(MOD_LALT, KC_QUOTE),                    MT(MOD_LALT, KC_QUOTE),  MT(MOD_LCTL, KC_EQUAL),    MT(MOD_LSFT, KC_DOT)
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
     [FKEYS_LAYOUT] = LAYOUT_moonlander(
       // traditional 10 key numpad plus fn-key access, general layout
-    KC_TRANSPARENT,         KC_F1,          KC_F2,          KC_F3,          KC_F4,                  KC_F5,          KC_TRANSPARENT,                 KC_TRANSPARENT, KC_F6, KC_F7,          KC_F8,          KC_F9,          KC_F10,          KC_TRANSPARENT,
+    KC_TRANSPARENT,         KC_F1,          KC_F2,          KC_F3,          KC_F4,                  KC_F5,          KC_TRANSPARENT,                 KC_TRANSPARENT, KC_F6, 			KC_F7,          KC_F8,          KC_F9,          KC_F10,          KC_TRANSPARENT,
     KC_TRANSPARENT,         S(KC_1),        S(KC_LBRC),     KC_LBRC,        S(KC_9),                S(KC_COMMA),    KC_TRANSPARENT,                 KC_TRANSPARENT, S(KC_DOT),      S(KC_0),        KC_RBRC,        S(KC_RBRC),     S(KC_0),         KC_TRANSPARENT,
     KC_TRANSPARENT,         S(KC_1),        S(KC_2),        S(KC_3),        S(KC_4),                S(KC_5),        S(KC_6),                        KC_TRANSPARENT, S(KC_6),        S(KC_7),        S(KC_8),        S(KC_9),        S(KC_0),         KC_TRANSPARENT,
-    KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         KC_TRANSPARENT,                                                 KC_BSLS,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
+    KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         KC_TRANSPARENT,                                                 KC_BSLS,        KC_EQUAL, 		KC_MINUS, 		KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
     KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TAB,                                 KC_TRANSPARENT,                 KC_TRANSPARENT,                 KC_SCLN,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
                                                                             MT(MOD_LSFT, KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, MT(MOD_LSFT, KC_SCLN)
   ),
