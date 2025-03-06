653aab1610c3d25747434f047418c78112a7376a
matt <dev@m.degregor.io>
2025-03-06 17:12:03 -0600

    more led fixes, shifting most used keys under middle finger -- for whatever reason i seem to be more comfortable using them for frequently pressed keys

--- keyboards/moonlander/keymaps/warpotato/layout_user.h
+++ keyboards/moonlander/keymaps/warpotato/layout_user.h
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,           KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                KC_TRANSPARENT,                           KC_BSLS,     KC_Y,   KC_U,        KC_I,                      KC_O,        KC_P,        KC_VOLU,
     KC_CAPS_LOCK,       KC_A,           KC_S,           KC_D,               TD(DNC_FIND),   KC_G,                KC_TRANSPARENT,                           KC_QUOTE,  KC_H,   KC_J,        KC_K,                      KC_L,        KC_SCLN,       KC_VOLD,
     KC_LSFT,            KC_Z,           TD(DNC_XCUT),   TD(DNC_COPY),       TD(DNC_CPS),    KC_B,                                                            KC_SLASH,   KC_N,        KC_M,                      KC_COMMA,    KC_TRANSPARENT,      KC_RSFT,
-    TT(NUMKEYS_LAYOUT), KC_LALT,        KC_TRANSPARENT, TT(NAV_LAYOUT),     MO(FKEYS_LAYOUT),           TD(DNC_BACKSPACE),                  TD(DNC_BACKSPACE),      MO(FKEYS_LAYOUT), LT(NAV_LAYOUT, KC_LBRC), KC_RBRC, KC_RALT, KC_LGUI,
+    TT(NUMKEYS_LAYOUT), KC_LALT,        KC_TRANSPARENT, MO(NAV_LAYOUT),     MO(FKEYS_LAYOUT),           TD(DNC_BACKSPACE),                  TD(DNC_BACKSPACE),      MO(FKEYS_LAYOUT), MO(NAV_LAYOUT), KC_TRANSPARENT, KC_RALT, KC_LGUI,
                                                         MT(MOD_LSFT, KC_SPC),      MT(MOD_LCTL, KC_ENTER),   MT(MOD_LALT, KC_QUOTE),                    MT(MOD_LALT, KC_QUOTE),  MT(MOD_LCTL, KC_EQUAL),    MT(MOD_LSFT, KC_SPC)
   ),  [OS_MAC_LAYOUT] = LAYOUT_moonlander(
       // minor modifications for mac os; most are handled via generic tapdance defs,1
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       // traditional 10 key numpad plus fn-key access, general layout
     KC_TRANSPARENT,         KC_F1,          KC_F2,          KC_F3,          KC_F4,                  KC_F5,           KC_TRANSPARENT,        KC_TRANSPARENT, KC_F6, 			KC_F7,          KC_F8,          KC_F9,          KC_F10,          KC_TRANSPARENT,
     KC_TRANSPARENT,         S(KC_1),        S(KC_LBRC),     KC_LBRC,        S(KC_9),                S(KC_COMMA),     KC_TRANSPARENT,        KC_TRANSPARENT, S(KC_DOT),      S(KC_0),        KC_RBRC,        S(KC_RBRC),     S(KC_0),         KC_TRANSPARENT,
-    KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_DOT,         KC_COMMA,               KC_BSLS,         KC_TRANSPARENT,        KC_TRANSPARENT, KC_SLASH,       KC_EQUAL, 		KC_MINUS, 		KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
+    KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_COMMA,         KC_DOT,               KC_BSLS,         KC_TRANSPARENT,        KC_TRANSPARENT, KC_SLASH,       KC_MINUS, 		KC_EQUAL, 		KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
     KC_TRANSPARENT,         S(KC_1),        S(KC_2),        S(KC_3),        S(KC_4),                S(KC_5),                                S(KC_6),        S(KC_7),        S(KC_8),        S(KC_9),        S(KC_0),         KC_TRANSPARENT,
     KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, S(KC_MINUS),                             KC_TRANSPARENT,        KC_TRANSPARENT,                 KC_SCLN,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
                                                                             MT(MOD_LSFT, KC_TAB),   KC_TRANSPARENT,  KC_TRANSPARENT,        KC_TRANSPARENT, KC_TRANSPARENT, MT(MOD_LSFT, KC_SCLN)
