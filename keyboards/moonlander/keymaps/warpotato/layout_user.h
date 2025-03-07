c08861dc08f041e633350a0903275a539314cfda
matt <dev@m.degregor.io>
2025-03-06 23:50:52 -0600

    make parentheses a bit easier to reach

--- keyboards/moonlander/keymaps/warpotato/layout_user.h
+++ keyboards/moonlander/keymaps/warpotato/layout_user.h
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FKEYS_LAYOUT] = LAYOUT_moonlander(
       // traditional 10 key numpad plus fn-key access, general layout
     KC_TRANSPARENT,         KC_F1,          KC_F2,          KC_F3,          KC_F4,                  KC_F5,           KC_TRANSPARENT,        KC_TRANSPARENT, KC_F6, 			KC_F7,          KC_F8,          KC_F9,          KC_F10,          KC_TRANSPARENT,
-    KC_TRANSPARENT,         S(KC_1),        S(KC_LBRC),     KC_LBRC,        S(KC_9),                S(KC_COMMA),     KC_TRANSPARENT,        KC_TRANSPARENT, S(KC_DOT),      S(KC_0),        KC_RBRC,        S(KC_RBRC),     S(KC_0),         KC_TRANSPARENT,
+    KC_TRANSPARENT,         S(KC_1),        S(KC_LBRC),     S(KC_9),        KC_LBRC,                S(KC_COMMA),     KC_TRANSPARENT,        KC_TRANSPARENT, S(KC_DOT),        KC_RBRC, S(KC_0),        S(KC_RBRC),     S(KC_0),         KC_TRANSPARENT,
     KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_COMMA,         KC_DOT,               KC_BSLS,         KC_TRANSPARENT,        KC_TRANSPARENT, KC_SLASH,       KC_MINUS, 		KC_EQUAL, 		KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
     KC_TRANSPARENT,         S(KC_1),        S(KC_2),        S(KC_3),        S(KC_4),                S(KC_5),                                S(KC_6),        S(KC_7),        S(KC_8),        S(KC_9),        S(KC_0),         KC_TRANSPARENT,
     KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, S(KC_MINUS),                             KC_TRANSPARENT,        KC_TRANSPARENT,                 KC_SCLN,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,  KC_TRANSPARENT,
