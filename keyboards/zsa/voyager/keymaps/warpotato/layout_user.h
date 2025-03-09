bc907f4d09a23b5ec20d3cbf45a0c03c66470bbe
matt <dev@m.degregor.io>
2025-03-09 09:58:55 -0500

    add quotes to symbol layer

--- keyboards/zsa/voyager/keymaps/warpotato/layout_user.h
+++ keyboards/zsa/voyager/keymaps/warpotato/layout_user.h
@@ -23,10 +23,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, MT(MOD_LGUI, KC_ENTER), MT(MOD_LGUI, KC_EQUAL), _______
         ),
     [FKEYS_LAYOUT] = LAYOUT(
-        _______, _______, _______,      KC_COMMA, KC_DOT,     KC_BSLS,             KC_SLASH,    KC_MINUS, KC_EQUAL, _______, _______,  _______,
+        _______, _______, KC_QUOTE,     KC_COMMA, KC_DOT,     KC_BSLS,             KC_SLASH,    KC_MINUS, KC_EQUAL, KC_QUOTE, _______,  _______,
         _______, S(KC_1), S(KC_2),      S(KC_3),  S(KC_4),    S(KC_5),             S(KC_7),     S(KC_8),  S(KC_9),  S(KC_0), _______, _______,
         _______, _______, S(KC_LBRC),   S(KC_9),  KC_LBRC,    S(KC_COMMA),         S(KC_DOT),   KC_RBRC,  S(KC_0),  S(KC_RBRC),S(KC_0),   _______,
-        _______, _______, _______,      _______, S(KC_MINUS), _______,              _______,     KC_SCLN,  _______, _______, _______, _______,
+        _______, _______, _______,      _______, S(KC_MINUS), _______,             _______,     KC_SCLN,  _______, _______, _______, _______,
         MT(MOD_LSFT, KC_TAB), _______,  _______, MT(MOD_LSFT, KC_SCLN)
         ),
     [NUM_LAYOUT] = LAYOUT(
