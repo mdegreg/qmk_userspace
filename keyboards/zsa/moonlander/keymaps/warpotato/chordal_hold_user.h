efcb91c65f8cebcb2dd831612440217a34704469
matt <dev@m.degregor.io>
2025-03-16 22:30:48 -0500

    adding easily accessible dot key, modifying chordal hold behaviors for thumb keys

--- keyboards/zsa/moonlander/keymaps/warpotato/chordal_hold_user.h
+++ keyboards/zsa/moonlander/keymaps/warpotato/chordal_hold_user.h
@@ -16,6 +16,6 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_moonla
     'L', 'L', 'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R', 'R', 'R',
     'L', 'L', 'L', 'L', 'L', 'L', 'L',      'R', 'R', 'R', 'R', 'R', 'R', 'R',
     'L', 'L', 'L', 'L', 'L', 'L',                'R', 'R', 'R', 'R', 'R', 'R',
-    'L', 'L', 'L', 'L', 'L',      'L',      'R',      'R', 'R', 'R', 'R', 'R',
-                        'L', 'L', 'L',      'R', 'R', 'R'
+    'L', 'L', 'L', 'L', 'L',      '*',      '*',      'R', 'R', 'R', 'R', 'R',
+                        '*', '*', '*',      '*', '*', '*'
   );
